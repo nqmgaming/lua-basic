e331bc270848e821218df519f50a26267b60cde5
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2025-01-06 21:58:37 +0700

    feat: too basic, I will speed video :)

--- src/main.lua
+++ src/main.lua
@@ -1,21 +1,10 @@
-local x = 8 -- only visible in this file
-_G.GlobalVar = 9 -- visible everywhere
-
-print(x) -- 8
-print(GlobalVar) --
-_G.assert(GlobalVar == 9)
-local str = "Hello"
-print(str)
-local bool = true
-print(bool)
-
-local longStr = [[
+local x = #"Hello World"
+local longString = [[
     This is a long string
     that spans multiple lines
 ]]
 
-print(longStr)
-
-local name, old, is18 = "John", 30, true
-name = "Minh"
-print("Hello my name is" .. name .. " and I am " .. old .. " years old. I am 18: " .. tostring(is18))
+local num = 42
+local numString = tostring(num)
+print(x .. "\n" .. longString .. "\n" .. numString .. type(numString))
+print("Hello World\t" .. "Hello World\n")
