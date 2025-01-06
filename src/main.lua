9cf91b2117806f9b020cd12aa77bb19dab1a0ebe
Nguyen Quang Minh <nguyenquangminh391@gmail.com>
2025-01-06 21:53:39 +0700

    feat: variable and data type

--- src/main.lua
+++ src/main.lua
@@ -1,2 +1,21 @@
--- print to console
-print("Hello world!" .. "I am coold") -- Hello world!
+local x = 8 -- only visible in this file
+_G.GlobalVar = 9 -- visible everywhere
+
+print(x) -- 8
+print(GlobalVar) --
+_G.assert(GlobalVar == 9)
+local str = "Hello"
+print(str)
+local bool = true
+print(bool)
+
+local longStr = [[
+    This is a long string
+    that spans multiple lines
+]]
+
+print(longStr)
+
+local name, old, is18 = "John", 30, true
+name = "Minh"
+print("Hello my name is" .. name .. " and I am " .. old .. " years old. I am 18: " .. tostring(is18))
